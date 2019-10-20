0024bb22651854e96d9a415c94dcdbe91ae9f122
Warren-Atchison <42626191+Warren-Atchison@users.noreply.github.com>
2019-10-19 20:51:49 -0400

    slight change to output list from youtube search function
    as summary

--- src/youtube.py
+++ src/youtube.py
@@ -4,8 +4,9 @@ import json
 api_key = json.load(open("../data/api.json"))["apiKey"]
 yt = YouTubeDataAPI(api_key)
 
-def search(searchQuery: str):
+def search(searchQuery: str) -> list:
     searches = yt.search(q=searchQuery, max_results=5)
     print("Retrieved: " + searches[0]["video_title"])
+    return searches
 
-search("disturbed down with the sickness")
+coolThing = search("disturbed down with the sickness")
