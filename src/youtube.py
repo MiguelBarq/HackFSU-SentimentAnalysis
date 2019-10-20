31f614171fd178a3d2497438389f859fdc31b65d
Warren-Atchison <42626191+Warren-Atchison@users.noreply.github.com>
2019-10-20 01:50:47 -0400

    added in error catching for failed api calls

--- src/youtube.py
+++ src/youtube.py
@@ -6,12 +6,12 @@ def search(searchQuery: str) -> list:
     api_key = json.load(open("../data/api.json"))["apiKey"]
     yt = YouTubeDataAPI(api_key)
 
-    searches = yt.search(q=searchQuery, max_results=5)
+    searches = yt.search(q=searchQuery)
     return searches
 
 
 # Function to retrieve video_id and generate a URL with it to return
-def getURL(searchQuery: str, searchResults:int = 100) -> str:
+def getURL(searchQuery: str, searchResults:int = 1) -> str:
     api_key = json.load(open("../data/api.json"))["apiKey"]
     yt = YouTubeDataAPI(api_key)
 
