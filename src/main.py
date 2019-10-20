d44f8c4a2fe454b219a184fcb1a6a57dd7fd34e4
Warren-Atchison <42626191+Warren-Atchison@users.noreply.github.com>
2019-10-20 05:30:22 -0400

    i

--- src/main.py
+++ src/main.py
@@ -2,26 +2,42 @@ import numpy as np
 import youtube
 import ui
 import vidinput
-import sentAnalysis
+# import sentAnalysis
 
 query_dict = {
     'rock' : 1,
-    'classical' : 1,
+    'instrumental' : 1,
     'pop' : 1,
-    'blues' : 1,
+    'jazz' : 1,
     'edm' : 1,
     'alternative' : 1
 }
 
+# General Logic Path:
+#   1. Open UI and generate elements
+#   2. Start sentiment analysis and analyze the webcam
+#   3. Pull a song from Youtube and play it based on user mood
+#   4. When song ends, analyze effectiveness of song and adjust probability table
+#   5. Fetch new song for user to neutralize mood
+#   6. Repeat.
+
+# General Logic Path, but BETTER:
+#   1. Open UI and generate elements
+#   2. Run sentiment analysis, record it.
+#   3. Play song depending on analysis
+#   4. Throughout the song, occasionally run sentiment analysis, constructing a list of emotions 
+#   5. At the end of the song, determine if mood improved overall over the course of the song
+#       a) Determine which emotions are "bad", put them in a list
+#       b) Determine if, towards end of sentiment analysis list, if mood improved
+#           i) If it got WORSE, maybe set the dict value to one less, or 0 if at 0 (so no negatives)?
+#   6. If it improved, add one to the dict value. If it didn't, do nothing
+#   7. Go back to 3
+
 def main():
-    effective = True
-    if effective:
-        query_dict['rock'] += 1
+    # Sentiment Analysis
+    mood = 'happy'# <TODO: sentAnalysis call (note: needs to return a string)    
 
-    # List of things to do tada
-    print(list(query_dict.keys()))
-    print([x/sum(query_dict.values()) for x in query_dict.values()])
-
-    print(np.random.choice(list(query_dict.keys()), 1, p = [x/sum(query_dict.values()) for x in query_dict.values()]))
+    # Create UI
+    ui.createUI(mood + ' ' + np.random.choice(list(query_dict.keys()), 1, p = [x/sum(query_dict.values()) for x in query_dict.values()])[0])
 
 main()
--- src/ui.py
+++ src/ui.py
@@ -3,29 +3,22 @@ import webbrowser
 # import youtube
 import pafy
 import vlc
+import time
+import sentAnalysis
 
-def createUI():
+player = []
+
+def createUI(searchQuery:str):
     ui = tk.Tk()
     ui.geometry("300x120")
     ui.wm_title("HackFSU - Music Player")
 
-    elements = createElements(ui)
+    elements = createElements(ui, searchQuery)
 
     ui.mainloop()
 
-def createElements(ui) -> list:
+def createElements(ui, searchQuery:str) -> list:
     outList = []
-
-    # bulding player to stream music on pc
-    try:
-        # url = youtube.getURL("happy music")
-        # print(url)
-        video = pafy.new("https://www.youtube.com/watch?v=NvZtkt9973A")
-        aStream = video.getbestaudio()
-        playurl = aStream.url
-        player = vlc.MediaPlayer(playurl)
-    except:
-        print("[WARNING] We were not able to retrieve a viable audio stream at this time from Youtube.")
 
     title = tk.Label(ui, text="Title, bruvh")
     title.pack(side="top")
@@ -39,14 +32,29 @@ def createElements(ui) -> list:
     pause.pack(side="bottom", fill="x")
     outList.append(pause)
 
-    button = tk.Button(text="play the video", command=lambda: playButton(player))
+    button = tk.Button(text="play the video", command=lambda: playButton(player, searchQuery))
     button.pack(side="bottom", fill="x")
     outList.append(button)
 
     return outList
 
+def setSong(searchQuery:str):
+    print("setSong attempting to search for: " + searchQuery)
 
-def playButton(player):
+    # bulding player to stream music on pc
+    try:
+        # url = youtube.getURL(searchQuery)
+        # print(url)
+        video = pafy.new("https://www.youtube.com/watch?v=BAGkYLoVFLM")
+        aStream = video.getbestaudio()
+        playurl = aStream.url
+        player = vlc.MediaPlayer(playurl)
+        loopyboi(player)
+    except:
+        print("[WARNING] We were not able to retrieve a viable audio stream at this time from Youtube.")
+
+def playButton(player, searchQuery:str):
+    setSong(searchQuery)
     player.play()
 
 def pauseButton(player):
@@ -55,4 +63,36 @@ def pauseButton(player):
 def stopButton(player):
     player.stop()
 
-createUI()
+# For you to place where needed
+def loopyboi(player):
+    # Holds the sentiment analysis for throughout the song
+    song_sent = []
+    # Tells when near end of song. TODO
+    end_of_song = False
+
+    # This loop will call sentiment analysis at 3fps once every 2 seconds
+    start_time = time.time()
+    song_start_time = time.time()
+    song_sent.append(sentAnalysis.sentAnalysis(3))
+    while True:
+        if end_of_song:
+            break
+        
+        # Updating time
+        curr_time = time.time()
+
+        # Checks if song is over
+        if(float(curr_time - song_start_time) >= player.get_length()):
+            end_of_song = True
+
+        # If two seconds have elapsed
+        if (float(curr_time - start_time) >= 2):
+            # Getting another sentiment analysis
+            song_sent.append(sentAnalysis.sentAnalysis(3))
+            # Updating start_time
+            start_time = time.time()
+
+    return song_sent
+
+
+    
